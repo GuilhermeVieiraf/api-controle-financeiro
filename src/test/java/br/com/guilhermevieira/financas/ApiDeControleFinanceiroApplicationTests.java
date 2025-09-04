ce01e922d65e1c2b63237150823e9b8225a3d8c0
GuilhermeVieiraf <guilhermevfelix06@gmail.com>
2025-09-04 17:56:25 -0300

    chore: Configuração inicial do projeto com dependências e ambiente

---
+++ src/test/java/br/com/guilhermevieira/financas/ApiDeControleFinanceiroApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.guilhermevieira.financas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiDeControleFinanceiroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
